b22fc3550a216cee2ec955b97ce64ef452c3720f
CAQUINEAU Aline <caquineaualine@live.fr>
2018-10-22 16:22:50 +0200

    termine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,200 +1,370 @@
-
 $(function(){
-
-var price = $(".price-container").val();
-var quantite = $(".quantite").val();
-var totalPrice = price*quantite
-
   $('#guitar').click(function(){
-    $('#card1').removeAttr('hidden', 'show');
+    var count = $('#qt1').val();
+    count++;
+    $('#qt1').val(count);
+    $('#card1').removeAttr('hidden');
+    $('#card1').removeClass('hidden').addClass('show');
   });
   $('#deletecard1').click(function(){
-    $('#card1').hide();
+    $('#card1').removeClass('show').addClass('hidden');
   });
   $('#guitar2').click(function(){
-    $('#card2').removeAttr('hidden', 'show');
+    var count = $('#qt2').val();
+    count++;
+    $('#qt2').val(count);
+    $('#card2').removeAttr('hidden');
+    $('#card2').removeClass('hidden').addClass('show')
   });
   $('#deletecard2').click(function(){
-    $('#card2').hide();
+    $('#card2').removeClass('show').addClass('hidden');
   });
   $('#guitar3').click(function(){
-    $('#card3').removeAttr('hidden', 'show');
+    var count = $('#qt3').val();
+    count++;
+    $('#qt3').val(count);
+    $('#card3').removeAttr('hidden');
+    $('#card3').removeClass('hidden').addClass('show')
   });
   $('#deletecard3').click(function(){
-    $('#card3').hide();
+    $('#card3').removeClass('show').addClass('hidden');
   });
   $('#guitar4').click(function(){
-    $('#card4').removeAttr('hidden', 'show');
+    var count = $('#qt4').val();
+    count++;
+    $('#qt4').val(count);
+    $('#card4').removeAttr('hidden');
+    $('#card4').removeClass('hidden').addClass('show')
   });
   $('#deletecard4').click(function(){
-    $('#card4').hide();
+    $('#card4').removeClass('show').addClass('hidden');
   });
   $('#bass').click(function(){
-    $('#card5').removeAttr('hidden', 'show');
+    var count = $('#qt5').val();
+    count++;
+    $('#qt5').val(count);
+    $('#card5').removeAttr('hidden');
+    $('#card5').removeClass('hidden').addClass('show')
   });
   $('#deletecard5').click(function(){
-    $('#card5').hide();
+    $('#card5').removeClass('show').addClass('hidden');
   });
   $('#bass2').click(function(){
-    $('#card6').removeAttr('hidden', 'show');
+    var count = $('#qt6').val();
+    count++;
+    $('#qt6').val(count);
+    $('#card6').removeAttr('hidden');
+    $('#card6').removeClass('hidden').addClass('show')
   });
   $('#deletecard6').click(function(){
-    $('#card6').hide();
+    $('#card6').removeClass('show').addClass('hidden');
   });
   $('#bass3').click(function(){
-    $('#card7').removeAttr('hidden', 'show');
+    var count = $('#qt7').val();
+    count++;
+    $('#qt7').val(count);
+    $('#card7').removeAttr('hidden');
+    $('#card7').removeClass('hidden').addClass('show')
   });
   $('#deletecard7').click(function(){
-    $('#card7').hide();
+    $('#card7').removeClass('show').addClass('hidden');
   });
   $('#bass4').click(function(){
-    $('#card8').removeAttr('hidden', 'show');
+    var count = $('#qt8').val();
+    count++;
+    $('#qt8').val(count);
+    $('#card8').removeAttr('hidden');
+    $('#card8').removeClass('hidden').addClass('show')
   });
   $('#deletecard8').click(function(){
-    $('#card8').hide();
+    $('#card8').removeClass('show').addClass('hidden');
   });
   $('#micro').click(function(){
-    $('#card9').removeAttr('hidden', 'show');
+    var count = $('#qt9').val();
+    count++;
+    $('#qt9').val(count);
+    $('#card9').removeAttr('hidden');
+    $('#card9').removeClass('hidden').addClass('show')
   });
   $('#deletecard9').click(function(){
-    $('#card9').hide();
+    $('#card9').removeClass('show').addClass('hidden');
   });
   $('#micro2').click(function(){
-    $('#card10').removeAttr('hidden', 'show');
+    var count = $('#qt10').val();
+    count++;
+    $('#qt10').val(count);
+    $('#card10').removeAttr('hidden');
+    $('#card10').removeClass('hidden').addClass('show')
   });
   $('#deletecard10').click(function(){
-    $('#card10').hide();
+    $('#card10').removeClass('show').addClass('hidden');
   });
   $('#micro3').click(function(){
-    $('#card11').removeAttr('hidden', 'show');
+    var count = $('#qt11').val();
+    count++;
+    $('#qt11').val(count);
+    $('#card11').removeAttr('hidden');
+    $('#card11').removeClass('hidden').addClass('show')
   });
   $('#deletecard11').click(function(){
-    $('#card11').hide();
+    $('#card11').removeClass('show').addClass('hidden');
   });
   $('#micro4').click(function(){
-    $('#card12').removeAttr('hidden', 'show');
+    var count = $('#qt12').val();
+    count++;
+    $('#qt12').val(count);
+    $('#card12').removeAttr('hidden');
+    $('#card12').removeClass('hidden').addClass('show')
   });
   $('#deletecard12').click(function(){
-    $('#card12').hide();
+    $('#card12').removeClass('show').addClass('hidden');
   });
   $('#speakers').click(function(){
-    $('#card13').removeAttr('hidden', 'show');
+    var count = $('#qt13').val();
+    count++;
+    $('#qt13').val(count);
+    $('#card13').removeAttr('hidden');
+    $('#card13').removeClass('hidden').addClass('show')
   });
   $('#deletecard13').click(function(){
-    $('#card13').hide();
+    $('#card13').removeClass('show').addClass('hidden');
   });
   $('#speakers2').click(function(){
-    $('#card14').removeAttr('hidden', 'show');
+    var count = $('#qt14').val();
+    count++;
+    $('#qt14').val(count);
+    $('#card14').removeAttr('hidden');
+    $('#card14').removeClass('hidden').addClass('show')
   });
   $('#deletecard14').click(function(){
-    $('#card14').hide();
+    $('#card14').removeClass('show').addClass('hidden');
   });
   $('#speakers3').click(function(){
-    $('#card15').removeAttr('hidden', 'show');
+    var count = $('#qt15').val();
+    count++;
+    $('#qt15').val(count);
+    $('#card15').removeAttr('hidden');
+    $('#card15').removeClass('hidden').addClass('show')
   });
   $('#deletecard15').click(function(){
-    $('#card15').hide();
+    $('#card15').removeClass('show').addClass('hidden');
   });
   $('#speakers4').click(function(){
-    $('#card16').removeAttr('hidden', 'show');
+    var count = $('#qt16').val();
+    count++;
+    $('#qt16').val(count);
+    $('#card16').removeAttr('hidden');
+    $('#card16').removeClass('hidden').addClass('show')
   });
   $('#deletecard16').click(function(){
-    $('#card16').hide();
+    $('#card16').removeClass('show').addClass('hidden');
   });
   $('#piano').click(function(){
-    $('#card17').removeAttr('hidden', 'show');
+    var count = $('#qt17').val();
+    count++;
+    $('#qt17').val(count);
+    $('#card17').removeAttr('hidden');
+    $('#card17').removeClass('hidden').addClass('show')
   });
   $('#deletecard17').click(function(){
-    $('#card17').hide();
+    $('#card17').removeClass('show').addClass('hidden');
   });
   $('#piano2').click(function(){
-    $('#card18').removeAttr('hidden', 'show');
+    var count = $('#qt18').val();
+    count++;
+    $('#qt18').val(count);
+    $('#card18').removeAttr('hidden');
+    $('#card18').removeClass('hidden').addClass('show')
   });
   $('#deletecard18').click(function(){
-    $('#card18').hide();
+    $('#card18').removeClass('show').addClass('hidden');
   });
   $('#piano3').click(function(){
-    $('#card19').removeAttr('hidden', 'show');
+    var count = $('#qt19').val();
+    count++;
+    $('#qt19').val(count);
+    $('#card19').removeAttr('hidden');
+    $('#card19').removeClass('hidden').addClass('show')
   });
   $('#deletecard19').click(function(){
-    $('#card19').hide();
+    $('#card19').removeClass('show').addClass('hidden');
   });
   $('#piano4').click(function(){
-    $('#card20').removeAttr('hidden', 'show');
+    var count = $('#qt20').val();
+    count++;
+    $('#qt20').val(count);
+    $('#card20').removeAttr('hidden');
+    $('#card20').removeClass('hidden').addClass('show')
   });
   $('#deletecard20').click(function(){
-    $('#card20').hide();
+    $('#card20').removeClass('show').addClass('hidden');
   });
   $('#synth').click(function(){
-    $('#card21').removeAttr('hidden', 'show');
+    var count = $('#qt21').val();
+    count++;
+    $('#qt21').val(count);
+    $('#card21').removeAttr('hidden');
+    $('#card21').removeClass('hidden').addClass('show')
   });
   $('#deletecard21').click(function(){
-    $('#card21').hide();
+    $('#card21').removeClass('show').addClass('hidden');
   });
   $('#synth2').click(function(){
-    $('#card22').removeAttr('hidden', 'show');
+    var count = $('#qt22').val();
+    count++;
+    $('#qt22').val(count);
+    $('#card22').removeAttr('hidden');
+    $('#card22').removeClass('hidden').addClass('show')
   });
   $('#deletecard22').click(function(){
-    $('#card22').hide();
+    $('#card22').removeClass('show').addClass('hidden');
   });
   $('#synth3').click(function(){
-    $('#card23').removeAttr('hidden', 'show');
+    var count = $('#qt23').val();
+    count++;
+    $('#qt23').val(count);
+    $('#card23').removeAttr('hidden');
+    $('#card23').removeClass('hidden').addClass('show')
   });
   $('#deletecard23').click(function(){
-    $('#card23').hide();
+    $('#card23').removeClass('show').addClass('hidden');
   });
   $('#synth4').click(function(){
-    $('#card24').removeAttr('hidden', 'show');
+    var count = $('#qt24').val();
+    count++;
+    $('#qt24').val(count);
+    $('#card24').removeAttr('hidden');
+    $('#card24').removeClass('hidden').addClass('show')
   });
   $('#deletecard24').click(function(){
-    $('#card24').hide();
+    $('#card24').removeClass('show').addClass('hidden');
   });
   $('#drums').click(function(){
-    $('#card25').removeAttr('hidden', 'show');
+    var count = $('#qt25').val();
+    count++;
+    $('#qt25').val(count);
+    $('#card25').removeAttr('hidden');
+    $('#card25').removeClass('hidden').addClass('show')
   });
   $('#deletecard25').click(function(){
-    $('#card25').hide();
+    $('#card25').removeClass('show').addClass('hidden');
   });
   $('#drums2').click(function(){
-    $('#card26').removeAttr('hidden', 'show');
+    var count = $('#qt26').val();
+    count++;
+    $('#qt26').val(count);
+    $('#card26').removeAttr('hidden');
+    $('#card26').removeClass('hidden').addClass('show')
   });
   $('#deletecard26').click(function(){
-    $('#card26').hide();
+    $('#card26').removeClass('show').addClass('hidden');
   });
   $('#drums3').click(function(){
-    $('#card27').removeAttr('hidden', 'show');
+    var count = $('#qt27').val();
+    count++;
+    $('#qt27').val(count);
+    $('#card27').removeAttr('hidden');
+    $('#card27').removeClass('hidden').addClass('show')
   });
   $('#deletecard27').click(function(){
-    $('#card27').hide();
+    $('#card27').removeClass('show').addClass('hidden');
   });
   $('#drums4').click(function(){
-    $('#card28').removeAttr('hidden', 'show');
-  });
+    var count = $('#qt28').val();
+    count++;
+    $('#qt28').val(count);
+    $('#card28').removeAttr('hidden');
+    $('#card28').removeClass('hidden').addClass('show')
+      });
   $('#deletecard28').click(function(){
-    $('#card128').hide();
+    $('#card28').removeClass('show').addClass('hidden');
+});
+  $('#commander').click(function(){
+    var pdt1 = 800;
+    var quantite = $('#qt1').val();
+    var totalPrice1 = pdt1*quantite;
+    var pdt2 = 1600;
+    var quantite = $('#qt2').val();
+    var totalPrice2= pdt2*quantite;
+    var pdt3 = 1000;
+    var quantite = $('#qt3').val();
+    var totalPrice3 = pdt3*quantite;
+    var pdt4 = 1250;
+    var quantite = $('#qt4').val();
+    var totalPrice4 = pdt4*quantite;
+    var pdt5 = 1100;
+    var quantite = $('#qt5').val();
+    var totalPrice5 = pdt5*quantite;
+    var pdt6 = 1600;
+    var quantite = $('#qt6').val();
+    var totalPrice6 = pdt6*quantite;
+    var pdt7 = 900;
+    var quantite = $('#qt7').val();
+    var totalPrice7 = pdt7*quantite;
+    var pdt8 = 2000;
+    var quantite = $('#qt8').val();
+    var totalPrice8 = pdt8*quantite;
+    var pdt9 = 1200;
+    var quantite = $('#qt9').val();
+    var totalPrice9 = pdt9*quantite;
+    var pdt10 = 5000;
+    var quantite = $('#qt10').val();
+    var totalPrice10 = pdt10*quantite;
+    var pdt11 = 5200;
+    var quantite = $('#qt11').val();
+    var totalPrice11 = pdt11*quantite;
+    var pdt12 = 3000;
+    var quantite = $('#qt12').val();
+    var totalPrice12 = pdt12*quantite;
+    var pdt13 = 6000;
+    var quantite = $('#qt13').val();
+    var totalPrice13 = pdt13*quantite;
+    var pdt14 = 2000;
+    var quantite = $('#qt14').val();
+    var totalPrice14 = pdt14*quantite;
+    var pdt15 = 7500;
+    var quantite = $('#qt15').val();
+    var totalPrice15 = pdt15*quantite;
+    var pdt16 = 10000;
+    var quantite = $('#qt16').val();
+    var totalPrice16 = pdt16*quantite;
+    var pdt17 = 6000;
+    var quantite = $('#qt17').val();
+    var totalPrice17 = pdt17*quantite;
+    var pdt18 = 12000;
+    var quantite = $('#qt18').val();
+    var totalPrice18 = pdt18*quantite;
+    var pdt19 = 8000;
+    var quantite = $('#qt19').val();
+    var totalPrice19 = pdt19*quantite;
+    var pdt20 = 9500;
+    var quantite = $('#qt20').val();
+    var totalPrice20 = pdt20*quantite;
+    var pdt21 = 3550;
+    var quantite = $('#qt21').val();
+    var totalPrice21 = pdt21*quantite;
+    var pdt22 = 15000;
+    var quantite = $('#qt22').val();
+    var totalPrice22 = pdt22*quantite;
+    var pdt23 = 9600;
+    var quantite = $('#qt23').val();
+    var totalPrice23 = pdt23*quantite;
+    var pdt24 = 100;
+    var quantite = $('#qt24').val();
+    var totalPrice24 = pdt24*quantite;
+    var pdt25 = 2800;
+    var quantite = $('#qt25').val();
+    var totalPrice25 = pdt25*quantite;
+    var pdt26 = 2400;
+    var quantite = $('#qt26').val();
+    var totalPrice26 = pdt26*quantite;
+    var pdt27 = 16000;
+    var quantite = $('#qt27').val();
+    var totalPrice27 = pdt27*quantite;
+    var pdt28 = 8800;
+    var quantite = $('#qt28').val();
+    var totalPrice28 = pdt28*quantite;
+    var totalPrice = (totalPrice1 + totalPrice2 + totalPrice3 + totalPrice4 + totalPrice5 + totalPrice5 + totalPrice6 + totalPrice7 + totalPrice8 + totalPrice9 + totalPrice10 + totalPrice11 + totalPrice12 + totalPrice13 + totalPrice14 + totalPrice15 + totalPrice16 + totalPrice17 + totalPrice18 + totalPrice19 + totalPrice20 + totalPrice21 + totalPrice22 + totalPrice23 + totalPrice24 + totalPrice25 + totalPrice26 + totalPrice27 + totalPrice28)
+    alert('Prix total de votre panier : ' + totalPrice + '€')
   });
-  $('#drums5').click(function(){
-    $('#card29').removeAttr('hidden', 'show');
-  });
-  $('#deletecard29').click(function(){
-    $('#card1').hide();
-  });
-  $('#drums6').click(function(){
-    $('#card30').removeAttr('hidden', 'show');
-  });
-  $('#deletecard30').click(function(){
-    $('#card30').hide();
-  });
-  $('#drums7').click(function(){
-    $('#card31').removeAttr('hidden', 'show');
-  });
-  $('#deletecard31').click(function(){
-    $('#card31').hide();
-  });
-  $('#drums8').click(function(){
-    $('#card32').removeAttr('hidden', 'show');
-  });
-  $('#deletecard32').click(function(){
-    $('#card32').hide();
-  });
-  });
+});
